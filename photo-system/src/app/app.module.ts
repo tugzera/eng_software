f16a0b1005b4fad311aee6d72994b99754f5ae34
tugzera <damascenobdm@gmail.com>
2019-04-05 08:52:48 -0300

    #bootstrap + começo do html

--- photo-system/src/app/app.module.ts
+++ photo-system/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
